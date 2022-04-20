9b5c6139c10a1b7248fb66ad7cd07d2b447892ac
keerthi2806 <keerthikrishna7098@gmail.com>
2022-04-20 21:56:36 +0530

    Day - 3

--- src/test/java/com/harman/base/WebDriverWrapper.java
+++ src/test/java/com/harman/base/WebDriverWrapper.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -16,9 +18,9 @@ public class WebDriverWrapper {
 	protected WebDriver driver;
 
 	@BeforeMethod
-	public void setup() {
-		String browserName = "edge";
-
+	@Parameters({"browser"})
+	public void setup(@Optional("ch")String browserName) {
+		
 		if (browserName.equalsIgnoreCase("edge")) {
 			WebDriverManager.edgedriver().setup();
 			driver = new EdgeDriver();
